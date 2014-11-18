c00a9b17bd425d44ff7b77bec97ed56b88bee6cd
Ana Tovalin <astrostar98@gmail.com>
2014-11-18 14:59:50 -0800

    Loaded map on platformer

---
+++ core/src/com/tovalina/platformer/view/GameScreen.java
@@ -0,0 +1,56 @@
+package com.tovalina.platformer.view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class GameScreen implements Screen {
+    public TiledMap map;
+    public OrthogonalTiledMapRenderer renderer;
+    public OrthographicCamera camera;
+
+    public GameScreen() {
+        map = new TmxMapLoader().load("map/level01.tmx");      //load level map from my assets
+        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);//renders map onto the screen and sets pixel length of tiles
+        camera = new OrthographicCamera(14f, 14f);            //sets view of camera to 14 tiles by 14 tiles
+    }
+
+    @Override
+    public void render(float delta) {
+        camera.update();           //updates camera view
+        renderer.setView(camera);  //sets camera to view rendered map
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
