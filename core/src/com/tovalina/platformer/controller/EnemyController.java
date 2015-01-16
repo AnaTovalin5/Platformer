ac25b8512d47468a5c50016aa9c80a218d655a83
Ana Tovalin <ana.tovalin@gmail.com>
2015-01-16 14:35:33 -0800

    Linked physics body to enemy

--- core/src/com/tovalina/platformer/controller/EnemyController.java
+++ core/src/com/tovalina/platformer/controller/EnemyController.java
@@ -19,4 +19,22 @@ public class EnemyController {
     public static void update(float deltaTime) {
         enemy.update(deltaTime);  //updates the player's position
     }
+
+    private static void handleInput() {
+        Vector2 velocity = enemy.physicsBody.getLinearVelocity();
+        Vector2 position = enemy.physicsBody.getPosition();
+
+        if (Math.abs(velocity.x) > MAX_VELOCITY) {
+            velocity.x = Math.signum(velocity.x) * MAX_VELOCITY;  //sets maximum velocity so it doesn't fly off the screen
+            enemy.physicsBody.setLinearVelocity(velocity.x, velocity.y);
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            enemy.physicsBody.applyLinearImpulse(VELOCITY, 0f, position.x, position.y, true);  //moves character to the right when right key is pressed
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            enemy.physicsBody.applyLinearImpulse(-VELOCITY, 0f, position.x, position.y, true);  //moves character to the left when right key is pressed
+        }
+    }
 }
--- core/src/com/tovalina/platformer/model/Enemy.java
+++ core/src/com/tovalina/platformer/model/Enemy.java
@@ -12,12 +12,20 @@ public class Enemy extends Sprite {
     public Enemy(Vector2 position, int width, int height, String sheetpath) {
         super(position, width, height, sheetpath);
 
+        BodyDef bodydefinition = new BodyDef();
+        bodydefinition.type = BodyDef.BodyType.DynamicBody;
+        bodydefinition.position.set(position);
+
+        physicsBody = LevelController.gameWorld.createBody(bodydefinition); //assign the body defs to the player
+        physicsBody.setUserData(this);//attach this class to the playerBody
+
         PolygonShape rectangleShape = new PolygonShape();
         rectangleShape.setAsBox(this.width / 2f, this.height / 2f, new Vector2(this.width / 2f, this.height / 2f), 0f);  //defines rectangle
 
         FixtureDef fixtureDefinition = new FixtureDef();
         fixtureDefinition.shape = rectangleShape;  //creates the shape
 
+        physicsBody.createFixture(fixtureDefinition);
         rectangleShape.dispose(); //deletes the shape
         animations.put("walk", spriteSheet.createAnimation(0,1, 0.3f));
     }
